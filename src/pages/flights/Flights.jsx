0c768b385939674f24e99bab685b0354358d873b
Geetika10122002 <geetika101202@gmail.com>
2024-05-25 08:16:15 +0530

    adding

---
+++ src/pages/flights/Flights.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import axios from 'axios';
+import { baseurl } from '../../util';
+
+
+
+const Flights = ({ data, ticket }) => {
+  const navigate = useNavigate();
+
+  const userdata = JSON.parse(localStorage.getItem('userData'));
+  console.log(userdata);
+
+  useEffect(() => {
+    if (userdata == null) {
+      navigate('/login')
+    }
+  }, []);
+  const success = () => {
+    confirmAlert({
+      title: 'Confirmation',
+      message: 'Flight has been added successfully',
+      buttons: [
+        {
+          label: 'Ok',
+          onClick: onclose
+        },
+
+      ]
+    });
+  };
+
+  const error = () => {
+    confirmAlert({
+      title: 'Error',
+      message: 'Error!occurred while adding the flight Try again',
+      buttons: [
+        {
+          label: 'Ok',
+          onClick: onclose
+        },
+
+      ]
+    });
+  };
+
+  const submit = (flight) => {
+    confirmAlert({
+      title: 'Confirm to submit',
+      message: `Are You Sure You Want to Book${flight.flightName}`,
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => handleBookFlight(flight)
+        },
+        {
+          label: 'No',
+          onClick: onclose
+        }
+      ]
+    });
+  };
+  const handleBookFlight = (flight) => {
+console.log(flight);
+
+    axios.post(`${baseurl}/bookTicket`, {
+      userId: userdata.userId,
+      flightId: flight.flightId,
+      fromDestination: flight.flightFrom,
+      toDestination: flight.flightTo,
+      noOfSeats: ticket,
+      date: flight.flightDate.slice(0, 10),
+      flightName: flight.flightName,
+      time:flight.flightTime
+    }).then((res) => {
+      console.log(res);
+      success();
+    }).catch((err) => {
+      console.log(err);
+      error();
+
+    })
+  };
+
+  console.log(data);
+
+  return (
+    <div className="flex items-center justify-center h-1/2 bg-sky-200 pb-10">
+      <div className="bg-white w-5/6 h-1/5 rounded-lg p-4 flex flex-col h-auto">
+        {data.map((flight, index) => (
+          <div key={index} className="flex flex-row p-4 w-full border border-gray-300 rounded-lg m-2   items-center justify-evenly">
+            <div className="flex flex-col">
+              <div className='text-2xl font-bold mb-2'>Flight Name</div>
+              <div>{flight.flightName}</div>
+            </div>
+            <div className="flex flex-col">
+              <div className='text-2xl font-bold mb-2'>Date</div>
+              <div>{flight.flightDate.slice(0, 10)}</div>
+            </div>
+            <div className="flex flex-col">
+              <div className='text-2xl font-bold mb-2'>Time</div>
+              <div>{flight.flightTime}</div>
+            </div>
+            <div className="flex flex-col">
+              <div className='text-2xl font-bold mb-2'>From</div>
+              <div>{flight.flightFrom}</div>
+            </div>
+            <div className="flex flex-col">
+              <div className='text-2xl font-bold mb-2'>To</div>
+              <div>{flight.flightTo}</div>
+            </div>
+            {/* <div className="flex flex-col">
+            <div className='text-2xl font-bold mb-2'>Rate per Person</div>
+            <div>{flight.ratePerPerson}</div>
+            </div> */}
+
+            <div>
+              <button
+                onClick={() => submit(flight)}
+                className="bg-green-500 text-white px-4 py-2 rounded-full focus:outline-none mt-4"
+              >
+                Book
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Flights;
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect,navigate } from 'react';
+import axios from 'axios';
+import { baseurl } from '../../util';
+import { useNavigate } from 'react-router-dom';
+
+
+const Flights = () => {
+  const [flightNameFilter, setFlightNameFilter] = useState('');
+  const [data, setData] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const navigate = useNavigate();
+
+
+  const handleFilter = () => {
+// console.log(userdata.userId);
+
+    axios.get(`${baseurl}/view/${flightNameFilter}`)
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+        setShowResults(true);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
+  const handleAddFlights = () => {
+  
+    navigate('/addflights');
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-sky-200">
+      <div className="bg-white w-5/6 h-1/5 rounded-lg p-4 flex flex-col h-auto">
+      <div className="mb-4">
+          <label htmlFor="flightName" className="text-xl font-bold mb-8">Filter Flights by Name:</label>
+          <div className="flex">
+            <input
+              type="text"
+              id="flightName"
+              value={flightNameFilter}
+              onChange={(e) => setFlightNameFilter(e.target.value)}
+              className="border-b border-gray-300 text-gray-900 sm:text-sm rounded-none w-full p-2.5"
+              placeholder="Enter Flight Name"
+            />
+              <div>
+          <button
+            onClick={handleFilter}
+            
+            className="bg-green-500 text-white px-4 py-2 rounded-full focus:outline-none mt-4"
+          >
+            Go
+          </button>
+
+        </div>
+
+        {/* Add Flights Button */}
+        <div>
+          <button
+            onClick={handleAddFlights}
+            className="bg-green-500  text-white px-4 py-2 rounded-full focus:outline-none mt-4"
+          >
+            Add 
+          </button>
+        </div>
+          </div>
+        </div>
+
+        {showResults && (
+          <div>
+            {data.length > 0 ? (
+              data.map((booking, index) => (
+                <div key={index} className="flex flex-row p-4 w-full border border-gray-300 rounded-lg m-2 items-center justify-evenly">
+                  <div className="flex flex-col">
+                    <div className='text-2xl font-bold mb-2'>User ID</div>
+                    <div>{booking.userId}</div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className='text-2xl font-bold mb-2'>Flight Name</div>
+                    <div>{booking.flightName}</div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className='text-2xl font-bold mb-2'>Date</div>
+                    <div>{booking.date.slice(0,10)}</div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className='text-2xl font-bold mb-2'>Time</div>
+                    <div>{booking.time}</div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className='text-2xl font-bold mb-2'>From</div>
+                    <div>{booking.fromDestination}</div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className='text-2xl font-bold mb-2'>To</div>
+                    <div>{booking.toDestination}</div>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className='text-2xl font-bold mb-2'>Total No of Seats</div>
+                    <div>{booking.noOfSeats}</div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div>No results to display.</div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Flights;
+
